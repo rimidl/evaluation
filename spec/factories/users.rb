844f2b98b906b77bd53664b5f0e8585ae66ef870
Evgrafov Denis <stereodenis@gmail.com>
2016-11-17 12:21:34 +0300

    erb to haml

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :user do
     confirmed_at Time.now
-    name "Test User"
-    email "test@example.com"
-    password "please123"
+    name 'Test User'
+    email 'test@example.com'
+    password 'please123'
 
     trait :admin do
       role 'admin'
